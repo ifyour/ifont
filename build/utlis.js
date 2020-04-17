30b4c0f6518bf32a37b957d1b905420fa09c6a67
Willard Wong <ifyour@outlook.com>
2020-04-17 19:01:53 +0800

    feat: optimizing regular expressions

--- build/utlis.js
+++ build/utlis.js
@@ -1,5 +1,6 @@
 function getFontName(req) {
-  const regex = /%.+\.ttf/g;
+  // const regex = /%.+\.ttf/g;
+  const regex = /([\u4e00-\u9fa5]|\w|\d|\%|\-)+\.ttf/gm;
   const content = req.url || '';
   const [matchContent] = content.match(regex) || [];
   return decodeURI(matchContent);
